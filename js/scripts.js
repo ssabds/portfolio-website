e893ec5d8989f5419086e512c8942c2d4038958b
ssabds <bergsonist@gmail.com>
2022-03-18 19:19:53 -0400

    added javascript & new menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log("Welcome To My Portfolio")
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
